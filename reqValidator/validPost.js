f99c1cf35c19ff63869aaf3e98ba8f52148a4543
Shubham <bakhalshubham@gmail.com>
2021-04-25 15:59:45 +0530

    Validate post updated with specific messages

--- reqValidator/validPost.js
+++ reqValidator/validPost.js
@@ -2,32 +2,27 @@ const { check, oneOf } = require('express-validator');
 
 exports.validCreate = [
   check('City', 'City is required')
-
     .isLength({
       max: 55,
     })
     .withMessage('Enter valid pin code for this city'),
   check('District', 'District is required')
-
     .isLength({
       max: 55,
     })
     .withMessage('Enter valid pin code for this district'),
   check('State', 'State is required')
-
     .isLength({
       max: 55,
     })
     .withMessage('Enter valid pin code for this state'),
   check('firstName', 'First Name is required')
-
     .isLength({
       min: 3,
       max: 32,
     })
     .withMessage('First Name must be between 3 to 32 characters'),
   check('lastName', 'Last Name is required')
-
     .isLength({
       min: 3,
       max: 32,
@@ -35,9 +30,18 @@ exports.validCreate = [
     .withMessage('Last Name must be between 3 to 32 characters'),
   check('contactNumber', 'Contact number is required').isMobilePhone(),
   check('otp', 'Enter OTP').isNumeric().withMessage('Invaid OTP Try again'),
-  check('blood_group').isIn(['O+', 'O-', 'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-']),
-  check('gender').isIn(['Male', 'Female', 'Other']),
-  check('Covid_Recovery_Date').trim().isDate(),
+  check('blood_group', 'Blood Group is required').isIn([
+    'O+',
+    'O-',
+    'A+',
+    'A-',
+    'B+',
+    'B-',
+    'AB+',
+    'AB-',
+  ]),
+  check('gender', 'Gender is required').isIn(['Male', 'Female', 'Other']),
+  check('Covid_Recovery_Date', 'Not a Valid date').trim().isDate(),
   check('age').custom(value => {
     if (parseInt(value) < 18) {
       return Promise.reject('Too younge to donate');
@@ -69,12 +73,14 @@ exports.validUpdate = [
     min: 4,
     max: 55,
   }),
-
-  check('blood_group')
+  check('blood_group', 'Blood Group is required')
     .optional()
     .isIn(['O+', 'O-', 'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-']),
-  check('gender').optional().isIn(['Male', 'Female', 'Other']),
-  check('Covid_Recovery_Date').optional().trim().isDate(),
+  check('gender', 'Gender is required')
+    .optional()
+    .isIn(['Male', 'Female', 'Other']),
+  check('Covid_Recovery_Date', 'Not a Valid date').optional().trim().isDate(),
+
   check('age')
     .optional()
     .custom(value => {
